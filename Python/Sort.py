ce969bf2524db9af3d9a45a1903a26b858bf8810
Mazawrath <Mazawrath@users.noreply.github.com>
2020-04-09 22:11:51 -0400

    Added best and worst case sorting

--- Python/Sort.py
+++ Python/Sort.py
@@ -37,6 +37,36 @@ for i in range(1, 100):
     timeElapsed = finish - start
     timings.append(timeElapsed)
     print("Time spent sorting " + str(i) + ".txt: " + str(timeElapsed))
+# Best case
+nums = []
+
+# Read the input in
+with open(f"..\\Test files\\Best\\best.txt") as file:
+    for line in file:
+        nums.append(int(line.rstrip()))
+file.close()
+
+# Sort the array
+timeElapsed = sort_array(nums)
+# Add timing to array of timings
+timings.append(timeElapsed)
+print("PYTHON: Time spent sorting best.txt: " + str(timeElapsed))
+
+# Worst case
+nums = []
+
+# Read the input in
+with open(f"..\\Test files\\Worst\\worst.txt") as file:
+    for line in file:
+        nums.append(int(line.rstrip()))
+file.close()
+
+# Sort the array
+timeElapsed = sort_array(nums)
+# Add timing to array of timings
+timings.append(timeElapsed)
+print("PYTHON: Time spent sorting worst.txt: " + str(timeElapsed))
+
 # Returns a datetime object containing the local date and time
 current_date_time = datetime.now()
 # Create the file
